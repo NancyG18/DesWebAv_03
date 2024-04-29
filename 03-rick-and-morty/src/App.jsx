1ef794639291dc5ec4febf303e2bbbcd3763315c
Nancy Gallegos <160012@mail.unacar.mx>
2024-04-29 15:26:14 -0600

    prueba

--- 03-rick-and-morty/src/App.jsx
+++ 03-rick-and-morty/src/App.jsx
@@ -1,29 +1,41 @@
 import { useState, useEffect } from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Inicio from './components/Inicio'
-import Personaje from './components/Personaje'
-import Imagenes from './components/Imagenes'
 import './App.css'
 
-
 function App () {
+  const [characters, setCharacters] = useState([])
+
+  const getApi = async () => {
+    try {
+      const res = await fetch('https://rickandmortyapi.com/api/character')
+      const data = await res.json()
+
+      console.log(data.results)
+
+      setCharacters(data.results)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    getApi()
+  }, [])
+
   return (
-    <div className='App'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Inicio></Inicio>}></Route>
-          <Route path='/personaje/:id' element={<Personaje></Personaje>}></Route>
-        </Routes>
-      </BrowserRouter>
+    <div className='w-11/12 m-auto'>
+      <h1 className='py-6 text-center text-3xl'>RICK AND MORTY</h1>
+      <div className='grid grid-cols-4 gap-4'>
+        {characters.map(character => (
+          <div key={character.id}>
+            <h2>{character.name}</h2>
+            <img src={character.image} alt="" />
+            <p>{character.species}</p>
+            <p>{character.status}</p>
+            <p>{character.gender}</p>
     </div>
-  )
-
-
-  return (
-    <>
-      <h1>RICK AND MORTY</h1>
-    </>
-  )
+        ))}
+        </div>
+      </div>
+  ) 
 }
 
 export default App
